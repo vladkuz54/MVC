e1c1656d8a735e062e76e3fd9a105b86b2553f5a
vladkuz54 <vkuz1540@gmail.com>
2026-05-18 19:35:07 +0300

    feat: implement frontend dashboard and authentication base functionality

--- presentation/routers/users_router.py
+++ presentation/routers/users_router.py
@@ -4,11 +4,10 @@ from fastapi import APIRouter, Depends, HTTPException
 from passlib.context import CryptContext
 from pydantic import BaseModel, ConfigDict
 
-from .auth import get_current_user
 from bll.services.users_service import UsersService
 
 from ..dependencies import get_users_service
-
+from .auth import get_current_user
 
 
 class UserResponse(BaseModel):
@@ -18,13 +17,17 @@ class UserResponse(BaseModel):
     organization_id: int
     model_config = ConfigDict(from_attributes=True)
 
+
 router = APIRouter(prefix="/users", tags=["users"])
-bcrypth_context = CryptContext(schemes=["bcrypt"], deprecated="auto")   
+bcrypth_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 @router.get("/", response_model=UserResponse)
-async def get_user_info(current_user: Annotated[dict, Depends(get_current_user)], service: UsersService = Depends(get_users_service)):
+async def get_user_info(
+    current_user: Annotated[dict, Depends(get_current_user)],
+    service: UsersService = Depends(get_users_service),
+):
     if not current_user:
         raise HTTPException(status_code=401, detail="Unauthorized")
-    
-    return await service.get_by_id(current_user["id"])
 
+    return await service.get_by_id(current_user["user_id"])
