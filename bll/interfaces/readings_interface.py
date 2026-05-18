4f33ac57edb69e7617784aa1a0c26b07d19710e9
vladkuz54 <vkuz1540@gmail.com>
2026-05-18 22:59:33 +0300

    feat: implement MVC pattern by adding BLL services, DAL repositories, and corresponding interfaces for system entities

--- bll/interfaces/readings_interface.py
+++ bll/interfaces/readings_interface.py
@@ -20,6 +20,10 @@ class IReadingsService(ABC):
         pass
 
     @abstractmethod
+    async def get_by_id_and_organization(self, id, organization_id):
+        pass
+
+    @abstractmethod
     async def create(self, data):
         pass
 
