2f19aaf55ed2eef1b025a9867b88175eb3086787
vladkuz54 <vkuz1540@gmail.com>
2026-05-18 22:59:56 +0300

    feat: implement full CRUD repositories, interfaces, and routers for MVC architecture components

--- dal/interfaces/organizations_interfaces.py
+++ dal/interfaces/organizations_interfaces.py
@@ -5,7 +5,6 @@ from .base_interfaces import IBaseRepository
 
 
 class IOrganizationsRepository(IBaseRepository[Organizations]):
-    pass
 
     @abstractmethod
     async def get_by_organization(self, organization_id):
--- dal/interfaces/readings_interfaces.py
+++ dal/interfaces/readings_interfaces.py
@@ -12,3 +12,7 @@ class IReadingsRepository(IBaseRepository[Readings]):
     @abstractmethod
     async def get_by_organization(self, organization_id):
         pass
+
+    @abstractmethod
+    async def get_by_id_and_organization(self, id, organization_id):
+        pass
